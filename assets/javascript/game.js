5fef6e3bd53a1a24c54092b3d1a340faf66b20d1
Jake Phillips <phillips.jakecolby@gmail.com>
2019-04-14 23:08:36 -0400

    final update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,19 +9,19 @@ $(document).ready(function() {
 
 	var treadmillOne = Math.floor(Math.random() * 12) + 1; 
 		console.log("Treadmill 1: " + treadmillOne); 
-		$("#img1").html("<img src=" + "../assets/images/image1.png" + " value=" + treadmillOne + ">"); 
+		$("#img1").html("<img src=" + "assets/images/image1.png" + " value=" + treadmillOne + ">"); 
 
 	var treadmillTwo = Math.floor(Math.random() * 12) + 1; 
 		console.log("Treadmill 2: " + treadmillTwo); 
-		$("#img2").html("<img src=" + "../assets/images/image2.png" + " value=" + treadmillTwo + ">"); 
+		$("#img2").html("<img src=" + "assets/images/image2.png" + " value=" + treadmillTwo + ">"); 
 
 	var treadmillThree = Math.floor(Math.random() * 12) + 1; 
 		console.log("Treadmill 3: " + treadmillThree); 
-		$("#img3").html("<img src=" + "../assets/images/image3.png" + " value=" + treadmillThree + ">");
+		$("#img3").html("<img src=" + "assets/images/image3.png" + " value=" + treadmillThree + ">");
 	
 	var treadmillFour = Math.floor(Math.random() * 12) + 1; 
 		console.log("Treadmill 4: " + treadmillFour); 
-		$("#img4").html("<img src=" + "../assets/images/image4.png" + " value=" + treadmillFour + ">");
+		$("#img4").html("<img src=" + "assets/images/image4.png" + " value=" + treadmillFour + ">");
 		
 	var wins = 0; 
 		console.log("wins: " + wins); 
@@ -42,19 +42,19 @@ $(document).ready(function() {
 
 		treadmillOne = Math.floor(Math.random() * 12) + 1;  
 			console.log("Treadmill 1: " + treadmillOne); 
-		$("#img1").html("<img src=" + "../assets/images/image1.png" + " value=" + treadmillOne + ">"); 
+		$("#img1").html("<img src=" + "assets/images/image1.png" + " value=" + treadmillOne + ">"); 
 
 		treadmillTwo = Math.floor(Math.random() * 12) + 1; 
 			console.log("Treadmill 2: " + treadmillTwo); 
-		$("#img2").html("<img src=" + "../assets/images/image2.png" + " value=" + treadmillTwo + ">"); 
+		$("#img2").html("<img src=" + "assets/images/image2.png" + " value=" + treadmillTwo + ">"); 
 
 		treadmillThree = Math.floor(Math.random() * 12) + 1; 
 			console.log("Treadmill 3: " + treadmillThree); 
-		$("#img3").html("<img src=" + "../assets/images/image3.png" + " value=" + treadmillThree + ">");
+		$("#img3").html("<img src=" + "assets/images/image3.png" + " value=" + treadmillThree + ">");
 	
 		treadmillFour = Math.floor(Math.random() * 12) + 1; 
 			console.log("Treadmill 4: " + treadmillFour); 
-		$("#img4").html("<img src=" + "../assets/images/image4.png" + " value=" + treadmillFour + ">");
+		$("#img4").html("<img src=" + "assets/images/image4.png" + " value=" + treadmillFour + ">");
 
 		$("img").on("click", function () {
 			var newScore = score += parseInt($(this).attr("value")); 
